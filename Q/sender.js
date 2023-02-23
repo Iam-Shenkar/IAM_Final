d44856a878dcabe9bcc400ef48971f17e5feaa79
tomerdu <YOUR-EMAIL>
2023-02-23 19:16:02 +0200

    feature exist function fixed + both Q`s works as expected

--- Q/sender.js
+++ Q/sender.js
@@ -4,34 +4,42 @@ const { amqpCreateFreePlan } = process.env;
 const { amqpSuspendedAccount } = process.env;
 
 const freePlan2Q = async (accountId) => {
-  amqp.connect(amqpCreateFreePlan, (err, conn) => {
-    conn.createChannel(async (error, ch) => {
-      const q = 'CloudAMQP';
-      const freePlan = {
-        accountId,
-      };
-      const stringMsg = JSON.stringify(freePlan);
-      ch.assertQueue(q, { durable: false });
-      await ch.sendToQueue(q, Buffer.from(stringMsg));
+  try {
+    amqp.connect(amqpCreateFreePlan, (err, conn) => {
+      conn.createChannel(async (error, ch) => {
+        const q = 'CloudAMQP';
+        const freePlan = {
+          accountId,
+        };
+        const stringMsg = JSON.stringify(freePlan);
+        ch.assertQueue(q, { durable: false });
+        await ch.sendToQueue(q, Buffer.from(stringMsg));
+      });
     });
-  });
+  } catch (error) {
+    console.error(`Error while sending message to amqpCreateFreePlan: ${error}`);
+  }
 };
 
 const newStatus2Q = async (accountId, status) => {
-  amqp.connect(amqpSuspendedAccount, (err, conn) => {
-    conn.createChannel(async (error, ch) => {
-      const q = 'CloudAMQP';
-      const suspendedAccount = {
-        accountId,
-        subscription: {
-          status,
-        },
-      };
-      const stringMsg = JSON.stringify(suspendedAccount);
-      ch.assertQueue(q, { durable: false });
-      await ch.sendToQueue(q, Buffer.from(stringMsg));
+  try {
+    amqp.connect(amqpSuspendedAccount, (err, conn) => {
+      conn.createChannel(async (error, ch) => {
+        const q = 'CloudAMQP';
+        const suspendedAccount = {
+          accountId,
+          subscription: {
+            status,
+          },
+        };
+        const stringMsg = JSON.stringify(suspendedAccount);
+        ch.assertQueue(q, { durable: false });
+        await ch.sendToQueue(q, Buffer.from(stringMsg));
+      });
     });
-  });
+  } catch (error) {
+    console.error(`Error while sending message to amqpSuspendedAccount: ${error}`);
+  }
 };
 
 module.exports = {

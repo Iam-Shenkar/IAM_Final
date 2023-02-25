e20fada52e626eb46c9501aa49ee80b638821fb0
Shahar <shaharariel95@gmail.com>
2023-02-26 00:51:18 +0200

    external login change

--- controllers/ExternalLoginController.js
+++ controllers/ExternalLoginController.js
@@ -2,23 +2,22 @@ const {
   User,
   Account,
 } = require('../repositories/repositories.init');
-const {statusCheck} = require("../services/authService");
-const {freePlan2Q} = require("../Q/sender");
+const { statusCheck } = require('../services/authService');
+const { freePlan2Q } = require('../Q/sender');
 
 const handleExternalCallBack = async (req, res) => {
   const findUser = await User.retrieve({ email: req.authInfo.email });
   const account = await Account.retrieve({ name: findUser.email });
-  if(req.authInfo.newFlag === 1){
+  if (req.authInfo.newFlag === 1) {
     await freePlan2Q(account._id.toString());
   }
-  await statusCheck(findUser, User)
+  await statusCheck(findUser, User);
   await Account.update({ _id: account._id.toString() }, { status: 'active' });
   await User.update({ email: findUser.email }, { accountId: account._id.toString(), status: 'active' });
   // cookies
   res.cookie('jwt', req.authInfo.refToken, {
     httpOnly: true,
     sameSite: 'None',
-    secure: true,
     maxAge: 24 * 60 * 60 * 1000,
   });
   await User.update(
@@ -28,11 +27,9 @@ const handleExternalCallBack = async (req, res) => {
       refreshToken: req.authInfo.refToken,
     },
   );
-  res.cookie('email', findUser.email);
-  res.cookie('name', findUser.name);
-  res.cookie('role', findUser.type);
-  res.cookie('account', findUser.accountId);
-  res.redirect('/');
+  res.status(200)
+    .json({ jwt: req.authInfo.refToken, role: findUser.type, email: findUser.email });
+  res.redirect('back');
 };
 
 module.exports = { handleExternalCallBack };

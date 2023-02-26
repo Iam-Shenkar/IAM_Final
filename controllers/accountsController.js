937235213ee94f85fdfadac777f0d9401a2bfd05
tomerdu <YOUR-EMAIL>
2023-02-26 17:40:09 +0200

    toggle

--- controllers/accountsController.js
+++ controllers/accountsController.js
@@ -73,7 +73,7 @@ const getAccount = async (req, res, next) => {
       accountId: req.params.id,
       name: acc.name,
       status: acc.status,
-      toggle: acc.toggle,
+      togglex: acc.toggle,
     };
     const merged = { ...outputArray, ...asset };
 
